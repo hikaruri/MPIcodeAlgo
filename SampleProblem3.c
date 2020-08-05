bb389a53106e750b062cd9471f15a6783d03279d
hikaruri <45931513+hikaruri@users.noreply.github.com>
2020-08-05 18:54:21 +0900

    Add files via upload

--- SampleProblem3.c
+++ SampleProblem3.c
@@ -28,26 +28,33 @@ int main(int argc, char *argv[]){
   double* procList;
   procList = (double*)malloc( sizeof(double) * Nproc );
 
+  int* procParamList;
+  procParamList = (int*)malloc( sizeof(int) * Nproc );
+
   int* procListNum;
   procListNum = (int*)malloc( sizeof(int) * numprocs );
 
   int procListindex = 0;
 
-  /* 条件を満たす値だけを持つリストを生成 */
+  /* 条件を満たす値だけを持つパラメータリストを生成 */
   for ( iproc = iprocINI; iproc < iprocEND; iproc++){
     srand((myid+iproc)*(myid+1000));
     double RandomNum = (double) rand() / RAND_MAX;
     if ( RandomNum > 0.9 ){
-      procList[procListindex] = RandomNum;
+      procList[procListindex]       = RandomNum;
+      procParamList[procListindex]  = iproc;
       procListindex++;
     }
   }
 
-  printf("myid:%d, ListNum=%d\n", myid, procListindex);
 
   double* procListGather;
   procListGather = (double*)malloc( sizeof(double) * procListindex );
   for ( int i = 0; i < procListindex; i++ ) procListGather[i] = procList[i];
+
+  int* procParamListGather;
+  procParamListGather = (int*)malloc( sizeof(int) * procListindex );
+  for ( int i = 0; i < procListindex; i++ ) procParamListGather[i] = procParamList[i];
 
   MPI_Barrier(MPI_COMM_WORLD);
 
@@ -62,6 +69,9 @@ int main(int argc, char *argv[]){
   double *List;
   List = (double*)malloc( sizeof(double) * ListCount );
 
+  int *ParamList;
+  ParamList = (int*)malloc( sizeof(int) * ListCount );
+
   MPI_Barrier(MPI_COMM_WORLD);
 
   /* Gatherv */
@@ -73,21 +83,30 @@ int main(int argc, char *argv[]){
     displs[i] =  displs[i-1] + procListNum[i-1];
   }
 
+  MPI_Barrier(MPI_COMM_WORLD);
+
   MPI_Gatherv( 
         procListGather, procListindex, MPI_DOUBLE,
         List,     procListNum, displs,
         MPI_DOUBLE, Host_ID, MPI_COMM_WORLD);
   
+  MPI_Gatherv( 
+        procParamListGather, procListindex, MPI_INT,
+        ParamList,     procListNum, displs,
+        MPI_INT, Host_ID, MPI_COMM_WORLD);
+  
   if ( myid == Host_ID ){
     for ( int i = 0; i < ListCount; i++ )
-      printf("List[%d] = %f\n", i, List[i]);
+      printf("List[%d] = %d %f\n", i, ParamList[i], List[i]);
   }
 
   
   free(procList);
   free(procListNum);
+  free(procParamList);
   free(displs);
   free(List);
+  free(ParamList);
 
   MPI_Finalize();
   return 0;
